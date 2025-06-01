56f33ca87be4233440f77cdfe31ec9e837138b59
eediallo <elhhabdouldiallo@gmail.com>
2025-06-01 21:24:44 +0100

    Add test for unfollowing a user

--- front-end/tests/profile.spec.mjs
+++ front-end/tests/profile.spec.mjs
@@ -1,8 +1,14 @@
-import {test, expect} from "@playwright/test";
-import {TIMELINE_USERNAMES_ELEMENTS_LOCATOR, loginAsSample, loginAsJustSomeGuy, waitForLocatorToHaveMatches} from "./test-utils.mjs";
+import { test, expect } from "@playwright/test";
+import {
+  TIMELINE_USERNAMES_ELEMENTS_LOCATOR,
+  loginAsSample,
+  loginAsJustSomeGuy,
+  waitForLocatorToHaveMatches,
+  signUp
+} from "./test-utils.mjs";
 
 test.describe("Profile View", () => {
-  test("shows own profile when logged in", async ({page}) => {
+  test("shows own profile when logged in", async ({ page }) => {
     // Given a profile view
     // When I am logged in as sample
     await loginAsSample(page);
@@ -16,8 +22,13 @@ test.describe("Profile View", () => {
       page.locator("#profile-container header a[data-username]")
     ).toBeVisible();
 
-    await waitForLocatorToHaveMatches(page, TIMELINE_USERNAMES_ELEMENTS_LOCATOR);
-    const postUsernames = new Set(await page.locator(TIMELINE_USERNAMES_ELEMENTS_LOCATOR).allInnerTexts());
+    await waitForLocatorToHaveMatches(
+      page,
+      TIMELINE_USERNAMES_ELEMENTS_LOCATOR
+    );
+    const postUsernames = new Set(
+      await page.locator(TIMELINE_USERNAMES_ELEMENTS_LOCATOR).allInnerTexts()
+    );
     expect(postUsernames).toEqual(new Set(["sample"]));
 
     // And bloom form is not attached
@@ -26,7 +37,7 @@ test.describe("Profile View", () => {
     ).not.toBeAttached();
   });
 
-  test("shows other user's profile with follow button", async ({page}) => {
+  test("shows other user's profile with follow button", async ({ page }) => {
     // Given I am logged in as AS
     await loginAsJustSomeGuy(page);
     // When I go to sample's profile
@@ -42,5 +53,31 @@ test.describe("Profile View", () => {
     await expect(page.locator("#timeline-container")).toBeVisible();
     // And bloom form is not attached
     await expect(page.locator("#bloom-form-container form")).not.toBeAttached();
+  });
+
+  test("allows unfollowing a user from their profile", async ({ page }) => {
+    await signUp(page, "sample");
+    await signUp(page, "AnotherUser");
+
+    // Given a profile component AnotherUser
+    // And I am logged in as sample
+    await loginAsSample(page);
+    await page.goto("/#/profile/AnotherUser");
+    // And sample is following AS
+    await page.click('[data-action="follow"]');
+
+    // When I view the profile component for AnotherUser
+    // Then I should see a button labeled "Unfollow"
+    const unfollowButton = page.locator('[data-action="unfollow"]');
+    await expect(unfollowButton).toBeVisible();
+
+    // When I click the "Unfollow" button
+    await unfollowButton.click();
+
+    // Then I should no longer be following AnotherUser
+    const followerCount = page.locator("[data-follower-count]");
+    await expect(followerCount).toHaveText("0");
+    // And the unfollow button is not visible
+    await expect(unfollowButton).toBe("hidden");
   });
 });
